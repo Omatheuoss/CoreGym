d45742a6aeba1e022f99a0ee8a3bac2d134091e1
Matheus <matheuspatrocinio258@gmail.com>
2024-01-10 14:48:49 -0300

    Projeto finalizado

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-// Obtenha a referência ao elemento do menu
+
 var menu = document.getElementById("menu");
 
 function clickMenu() {
-    // Alterne a propriedade de exibição do menu
-    if (menu.style.display === 'block') {
+    
         menu.style.display = 'none';
     } else {
         menu.style.display = 'block';
